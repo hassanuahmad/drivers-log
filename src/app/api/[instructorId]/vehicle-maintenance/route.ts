9761a27baed88f1fb92487b9a79c6b77315cc4ec
hassanuahmad <ahmad12p@uwindsor.ca>
2023-05-13 02:57:28 -0400

    Make POST req for vehicle-maintenance in backend

---
+++ src/app/api/[instructorId]/vehicle-maintenance/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+interface Request {
+    date: string;
+    odometer: number;
+    fueling: number;
+    gas: number;
+    maintenance: number;
+    remarks: string;
+}
+
+export async function POST(request: Request) {
+    const { date, odometer, fueling, gas, maintenance, remarks } = request;
+
+    // Add code to save the vehicle maintenance record to the database.
+
+    return NextResponse.json({ message: "Vehicle maintenance added." });
+}

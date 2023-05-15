7089d06644d98f336d3a2e8fbf70738bed495ad3
hassanuahmad <ahmad12p@uwindsor.ca>
2023-05-15 02:53:22 -0400

    Complete the lesson backend to save the req to db

--- app/api/[instructorId]/lesson/route.ts
+++ app/api/[instructorId]/lesson/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 interface Request {
-    selectStudent: string;
     date: string;
     startTime: string;
     endTime: string;
@@ -13,7 +15,6 @@ interface Request {
 
 export async function POST(request: any) {
     const {
-        selectStudent,
         date,
         startTime,
         endTime,
@@ -23,20 +24,25 @@ export async function POST(request: any) {
         remarks,
     } = await request.json();
 
-    console.log(
-        selectStudent,
-        date,
-        startTime,
-        endTime,
-        paymentType,
-        paymentAmount,
-        roadTest,
-        remarks
-    );
-
     // Also add code to calulate the duration of the lesson and add it to the database.
 
-    // Add code to save the lesson record to the database.
+    // Add code to get the instructorId and studentId from the URL instead of manually adding it.
 
-    return NextResponse.json({ message: "Lesson added." });
+    // Add code to save the lesson record to the database.
+    const record = await prisma.lesson.create({
+        data: {
+            date: date,
+            startTime: startTime,
+            endTime: endTime,
+            duration: 120,
+            paymentType: paymentType,
+            paymentAmount: paymentAmount,
+            roadTest: roadTest,
+            remarks: remarks,
+            studentId: 1,
+            instructorId: 1,
+        },
+    });
+
+    return NextResponse.json({ message: "Lesson added.", record });
 }

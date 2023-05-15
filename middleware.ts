b456a4e0b20e43f0f88b7b92db9bd34827b279ef
Nouman <noumancodes@gmail.com>
2023-05-15 17:23:32 -0400

    Removed unused middleware config

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,6 @@ export default authMiddleware();
 // TODO: This currently makes all pages protected with login
 // TODO: - Fix this so then we have public pages and private pages
 
-export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+// export const config = {
+//     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };

71a2705fea30c9014dd34544ff3b803874b62a24
Nouman <noumancodes@gmail.com>
2023-05-16 13:25:11 -0400

    Added dashboard route and layout for instructor

---
+++ app/instructor/dashboard/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Pricing() {
+    return <p>I am instructor dashboard page!</p>;
+}
---
+++ app/instructor/layout.tsx
@@ -0,0 +1,7 @@
+export default function VehicleMaintenanceLayout({
+    children, // will be a page or nested layout
+}: {
+    children: React.ReactNode;
+}) {
+    return <section>{children}</section>;
+}

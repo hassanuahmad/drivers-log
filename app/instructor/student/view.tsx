f56df3d9e4d310b5bacfaf42ba79294d9c378d35
hassanuahmad <ahmad12p@uwindsor.ca>
2023-05-19 18:39:25 -0400

    Add Delete functionality to Students- FE & backend

--- app/instructor/student/view.tsx
+++ app/instructor/student/view.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import KebabMenu from "../../components/kebabMenu";
+import DeleteModal from "../../components/deleteModal";
 
 type Record = {
     id: number;
@@ -20,6 +21,11 @@ type Record = {
 
 export default function View() {
     const [records, setRecords] = useState<Record[]>([]);
+    const [deleteRecord, setDeleteRecord] = useState<{
+        id: number;
+        endpoint: string;
+    } | null>(null);
+    const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
 
     useEffect(() => {
         fetch("/api/1/student")
@@ -31,7 +37,18 @@ export default function View() {
     }, []);
 
     const handleDelete = (id: number, endpoint: string) => {
-        console.log("DELETE", id, endpoint);
+        setDeleteRecord({ id, endpoint });
+        setDeleteModalOpen(true);
+    };
+
+    const handleDeleteConfirmed = () => {
+        if (deleteRecord) {
+            const newRecords = records.filter(
+                (record) => record.id !== deleteRecord.id
+            );
+            setRecords(newRecords);
+            setDeleteModalOpen(false);
+        }
     };
 
     const handleEdit = (id: number, endpoint: string) => {
@@ -40,111 +57,111 @@ export default function View() {
     };
 
     return (
-        <div className="px-4 sm:px-6 lg:px-8">
-            <div className="mt-8 flow-root">
-                <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div className="inline-block min-w-full py-2 align-middle ">
-                        <table className="min-w-full divide-y divide-gray-300">
-                            <thead>
-                                <tr>
-                                    <th
-                                        scope="col"
-                                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3"
-                                    >
-                                        #
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                                    >
-                                        First Name
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                                    >
-                                        Last Name
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                                    >
-                                        Phone Number
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                                    >
-                                        Email
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                                    >
-                                        Address
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                                    >
-                                        Class
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                                    >
-                                        BDE
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                                    >
-                                        Remarks
-                                    </th>
-
-                                    <th
-                                        scope="col"
-                                        className="relative py-3.5 pl-3 pr-4 sm:pr-3"
-                                    ></th>
-                                </tr>
-                            </thead>
-                            <tbody className="bg-white">
-                                {records.map((record, index) => (
-                                    <tr key={index} className="even:bg-gray-50">
-                                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
-                                            {index + 1}
-                                        </td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            {record.firstName}
-                                        </td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            {record.lastName}
-                                        </td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            {record.phoneNumber}
-                                        </td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            {record.email}
-                                        </td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            {record.streetAddress} {record.city}{" "}
-                                            {record.province}{" "}
-                                            {record.postalCode} {record.country}
-                                        </td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            {record.drivingClass}
-                                        </td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            {record.bde}
-                                        </td>
-                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            {record.remarks}
-                                        </td>
-                                        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
-                                            <a
-                                                href="#"
-                                                className="text-indigo-600 hover:text-indigo-900"
-                                            >
+        <>
+            <div className="px-4 sm:px-6 lg:px-8">
+                <div className="mt-8 flow-root">
+                    <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div className="inline-block min-w-full py-2 align-middle ">
+                            <table className="min-w-full divide-y divide-gray-300">
+                                <thead>
+                                    <tr>
+                                        <th
+                                            scope="col"
+                                            className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3"
+                                        >
+                                            #
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                        >
+                                            First Name
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                        >
+                                            Last Name
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                        >
+                                            Phone Number
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                        >
+                                            Email
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                        >
+                                            Address
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                        >
+                                            Class
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                        >
+                                            BDE
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                        >
+                                            Remarks
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="relative py-3.5 pl-3 pr-4 sm:pr-3"
+                                        ></th>
+                                    </tr>
+                                </thead>
+                                <tbody className="bg-white">
+                                    {records.map((record, index) => (
+                                        <tr
+                                            key={index}
+                                            className="even:bg-gray-50"
+                                        >
+                                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+                                                {index + 1}
+                                            </td>
+                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                {record.firstName}
+                                            </td>
+                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                {record.lastName}
+                                            </td>
+                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                {record.phoneNumber}
+                                            </td>
+                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                {record.email}
+                                            </td>
+                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                {record.streetAddress}{" "}
+                                                {record.city} {record.province}{" "}
+                                                {record.postalCode}{" "}
+                                                {record.country}
+                                            </td>
+                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                {record.drivingClass}
+                                            </td>
+                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                {record.bde}
+                                            </td>
+                                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                                {record.remarks}
+                                            </td>
+                                            <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
                                                 <KebabMenu
                                                     onDelete={() =>
                                                         handleDelete(
@@ -159,15 +176,25 @@ export default function View() {
                                                         )
                                                     }
                                                 />
-                                            </a>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            {/* Delete Modal */}
+            {deleteRecord && (
+                <DeleteModal
+                    isOpen={isDeleteModalOpen}
+                    onCancel={() => setDeleteModalOpen(false)}
+                    onDeleteConfirm={handleDeleteConfirmed}
+                    id={deleteRecord.id}
+                    endpoint={deleteRecord.endpoint}
+                />
+            )}
+        </>
     );
 }

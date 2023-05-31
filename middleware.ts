98e3f4b21e5c8f83d6b2ffa170ac2da0780325a9
Nouman <noumancodes@gmail.com>
2023-05-30 23:36:02 -0400

    /homepage is now public

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+    publicRoutes: ["/homepage"],
+});
 
 export const config = {
     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],

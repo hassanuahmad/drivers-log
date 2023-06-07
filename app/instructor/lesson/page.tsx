73e61e4bfc9bcc858ec1e9c70d55ad1b5e132bc7
hassanuahmad <ahmad12p@uwindsor.ca>
2023-06-07 00:38:45 -0400

    added empty option to then choose a student

--- app/instructor/lesson/page.tsx
+++ app/instructor/lesson/page.tsx
@@ -125,6 +125,7 @@ export default function Page() {
                                             autoComplete="selectStudent"
                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                                         >
+                                            <option value=""></option>
                                             {records.map((record) => (
                                                 <option
                                                     key={record.student.id}

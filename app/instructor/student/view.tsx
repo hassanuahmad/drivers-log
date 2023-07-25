db9cd5c0f08bfafedf8e75b638b21bd9009b4dfe
Nouman <noumancodes@gmail.com>
2023-07-25 00:52:28 -0400

    tailwind ui component

--- app/instructor/student/view.tsx
+++ app/instructor/student/view.tsx
@@ -79,17 +79,28 @@ export default function View() {
 
     return (
         <>
-            <form>
-                <label htmlFor="input">
-                    Search Name:{" "}
-                    <input
-                        placeholder="Name"
-                        onChange={(e) => {
-                            setSearchName(e.target.value);
-                        }}
-                    />
+            <div>
+                <label
+                    htmlFor="search"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                    Quick search
                 </label>
-            </form>
+                <div className="relative mt-2 flex items-center">
+                    <input
+                        type="text"
+                        name="search"
+                        id="search"
+                        className="block w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        onChange={(e) => setSearchName(e.target.value)}
+                    />
+                    <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
+                        <kbd className="inline-flex items-center rounded border border-gray-200 px-1 font-sans text-xs text-gray-400">
+                            ⌘S
+                        </kbd>
+                    </div>
+                </div>
+            </div>
             <div className="px-4 sm:px-6 lg:px-8">
                 <div className="mt-8 flow-root">
                     <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">

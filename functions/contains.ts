681f865a0eb87fd2df55ce5f8886378f2bb390ee
Nouman <noumancodes@gmail.com>
2023-07-26 15:47:25 -0400

    Stated contains helper functions and tsdoc

---
+++ functions/contains.ts
@@ -0,0 +1,10 @@
+/**
+ * Compares toCheck, the array of strings, to the Search string
+ * To search, the Search string, will be split up by spaces, then each of those parts will be matched with all elements in the toCheck array
+ * @param toCheck An array of Strings, that you would like to search
+ * @param search A String that will be split up by spaces to search with
+ * @returns True if the Search String is a substring in any element inside toCheck
+ */
+export let contains = (toCheck: Array<String>, search: String): Boolean => {
+    return false;
+};

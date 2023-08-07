02cb6fba60b1331095baf052f99954334fde7ba2
hassanuahmad <ahmad12p@uwindsor.ca>
2023-08-06 21:52:37 -0400

    When adding new record in VM, total doesn't change

--- app/instructor/vehicle-maintenance/page.tsx
+++ app/instructor/vehicle-maintenance/page.tsx
@@ -5,6 +5,7 @@ import * as Yup from "yup";
 import Notification from "../../components/notification";
 import { InstructorIdContext, InstructorIdContextType } from "../layout";
 import { VehicleMaintenanceRecordsContext } from "../../context/recordsContext";
+import { calculateTotals } from "./utils";
 
 interface VehicleMaintenanceFormValues {
     date: string;
@@ -26,7 +27,9 @@ const validationSchema = Yup.object({
 
 export default function Page() {
     // @ts-ignore
-    const { setRecords } = useContext(VehicleMaintenanceRecordsContext);
+    const { setRecords, setTotalGas, setTotalMaintenance } = useContext(
+        VehicleMaintenanceRecordsContext
+    );
     const { instructorId }: InstructorIdContextType =
         useContext(InstructorIdContext);
 
@@ -65,6 +68,15 @@ export default function Page() {
                 const newRecord = await response.json();
                 // @ts-ignore
                 setRecords((prevRecords) => [...prevRecords, newRecord.record]);
+                setTotalGas(
+                    // @ts-ignore
+                    (prevTotalGas) => prevTotalGas + newRecord.record.gas
+                );
+                setTotalMaintenance(
+                    // @ts-ignore
+                    (prevTotalMaintenance) =>
+                        prevTotalMaintenance + newRecord.record.maintenance
+                );
                 setShowNotification(true);
                 resetForm();
 

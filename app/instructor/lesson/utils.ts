7f4c28716a4c8b27e1f68472f47d7647c3cb52f4
hassanuahmad <ahmad12p@uwindsor.ca>
2023-08-28 17:10:58 -0400

    Add button "Download CSV" to download lessons

--- app/instructor/lesson/utils.ts
+++ app/instructor/lesson/utils.ts
@@ -47,3 +47,75 @@ export function calculateTotalPayment(
         return total;
     }, 0);
 }
+
+// @ts-ignore
+function convertRecordsToCSV(records) {
+    // Define the header of our CSV
+    const header = [
+        "#",
+        "Student Name",
+        "Date",
+        "Start Time",
+        "End Time",
+        "Duration",
+        "Payment Amount",
+        "Payment Type",
+        "Road Test",
+        "BDE",
+        "Lesson Remarks",
+        "",
+        "Student First Name",
+        "Student Last Last",
+        "Student Phone Number",
+        "Student Email",
+        "Student Address",
+        "Student Driving Class",
+        "Student Remarks",
+    ];
+
+    // Convert the records to CSV format
+    // @ts-ignore
+    const csvRecords = records.map((record, index) => [
+        index + 1,
+        `"${record.student.firstName} ${record.student.lastName}"`,
+        `"${record.date}"`,
+        `"${record.startTime}"`,
+        `"${record.endTime}"`,
+        `"${formatDuration(record.duration)}"`,
+        `"${record.paymentAmount}"`,
+        `"${record.paymentType}"`,
+        `"${record.roadTest}"`,
+        `"${record.student.bde}"`,
+        `"${record.remarks}"`,
+        " ",
+        `"${record.student.firstName}"`,
+        `"${record.student.lastName}"`,
+        `"${record.student.phoneNumber}"`,
+        `"${record.student.email}"`,
+        `"${record.student.streetAddress} ${record.student.city} ${record.student.province} ${record.student.postalCode} ${record.student.country}"`,
+        `"${record.student.drivingClass}"`,
+        `"${record.student.remarks}"`,
+    ]);
+
+    // Convert the arrays to CSV format
+    const csvData = [header, ...csvRecords]
+        .map((row) => row.join(","))
+        .join("\n");
+
+    return csvData;
+}
+
+// Download CSV
+// @ts-ignore
+export function downloadCSV(records, filename) {
+    const csvData = convertRecordsToCSV(records);
+    const blob = new Blob([csvData], { type: "text/csv" });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.setAttribute("hidden", "");
+    a.setAttribute("href", url);
+    a.setAttribute("download", filename);
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}

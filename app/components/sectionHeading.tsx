ce8747184c26a620e297d261fc0f1125995004ab
hassanuahmad <ahmad12p@uwindsor.ca>
2023-09-28 19:18:32 -0400

    Use shadcn in yearly data

--- app/components/sectionHeading.tsx
+++ app/components/sectionHeading.tsx
@@ -3,7 +3,7 @@ import {SectionHeadingProps} from "@/app/types/components/sectionHeading";
 export default function SectionHeading({title, description}: SectionHeadingProps) {
     return (
         <>
-            <div className="border-b border-gray-200 py-5">
+            <div className="border-b border-gray-200 py-6">
                 <h3 className="text-base font-semibold leading-6 text-gray-900">{title}</h3>
                 <p className="mt-2 max-w-4xl text-sm text-gray-500">
                     {description}
